19fba0ad227345fa7f98ccdee6bba5b7da4fd47a
Benjamin Malley <benjamin.malley@gmail.com>
2012-02-16 14:05:49 -0500

    added maps_api_key to the template

--- onmyway.py
+++ onmyway.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-import urllib2 as url
+from config import maps_api_key
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 @app.route('/', methods=['GET',])
 def index():
-	return render_template('index.html')
+	return render_template('index.html', maps_api_key=maps_api_key)
 	
 if __name__ == '__main__':
 	app.run(debug=True)
